464b435c8491054e4fa12ccb6fc95cc6e89c351c
Vlad Bokov <razum2um@mail.ru>
2018-02-17 03:15:23 +0700

    rewrite rewritten by rewrite

--- src/cljsh/repl.clj
+++ src/cljsh/repl.clj
@@ -1,5 +1,6 @@
-(clojure.core/ns cljsh.repl)
-(require '[rewrite-clj.zip :as z]) ;; manual
+(ns cljsh.repl
+  (:require [cljsh.postclj :as postclj]
+            [rewrite-clj.zip :as z]))
 
 (clojure.core/defn walk1 [inner outer form] (cond (list? form) (with-meta (outer (apply list (map inner form))) (meta form)) (instance? clojure.lang.IMapEntry form) (with-meta (outer (vec (map inner form))) (meta form)) (seq? form) (with-meta (outer (doall (map inner form))) (meta form)) (instance? clojure.lang.IRecord form) (with-meta (outer (reduce (fn [r x] (conj r (inner x))) form form)) (meta form)) (coll? form) (with-meta (outer (into (empty form) (map inner form))) (meta form)) :else (outer form)))
 
@@ -13,7 +14,31 @@
 
 (clojure.core/defn var->sym-filename [v] (let [sym (.sym v) ns (.name (.ns v)) fname (str (clojure.string/replace (clojure.string/replace ns #"\." "/") #"-" "_") ".clj")] (or (file-in-clsp fname) (str "src/" fname))))
 
-(clojure.core/defn rewrite [v] (when-let [upd (some-> v var->sym-filename z/of-file (z/find-token z/next (fn [n] (and (-> n z/sexpr #{(quote defn) (quote def) (quote clojure.core/defn) (quote clojure.core/def) (quote defnc) (quote defc)}) (-> n z/right z/sexpr (= (.sym v)))))) z/up (z/replace (-> v meta :code)))] (spit (-> v var->sym-filename) (z/root-string upd)) (z/sexpr upd)))
+(clojure.core/defn rewrite [v]
+  (when-let [upd (some->
+                   v
+                   var->sym-filename
+                   z/of-file
+                   (z/find-token
+                     z/next
+                     (fn [n]
+                       (and
+                         (-> n
+                          z/sexpr
+                          #{'clojure.core/def 'defn 'defc
+                            'clojure.core/defn 'def 'defnc})
+                         (-> n z/right z/sexpr (= (.sym v))))))
+                   z/up
+                   (z/replace
+                     (-> v
+                      meta
+                      :code
+                      postclj/pprint
+                      with-out-str
+                      z/of-string
+                      z/node)))]
+    (spit (-> v var->sym-filename) (z/root-string upd))
+    (z/sexpr upd)))
 
 (clojure.core/defn save [v] (let [sym-filename (->> v var->sym-filename)] (clojure.java.io/make-parents sym-filename) (when-not (-> sym-filename clojure.java.io/file .exists) (spit sym-filename (-> v meta :ns-code))) (when-not (rewrite v) (spit sym-filename "\n\n" :append true) (spit sym-filename (-> v meta :code) :append true))))
 
