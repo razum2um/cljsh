97288341fb4681cbc7b85aabc243faa5b216c314
Vlad Bokov <razum2um@mail.ru>
2018-02-17 04:24:27 +0700

    rewrite cljsh.postclj using cljsh.repl/rewrite-defn

--- src/cljsh/postclj.clj
+++ src/cljsh/postclj.clj
@@ -1,4 +1,7 @@
 (ns cljsh.postclj
   (:require [clojure.pprint :as pprint]))
 
-(clojure.core/defn pprint [code] (pprint/with-pprint-dispatch pprint/code-dispatch (pprint/pprint code)))
+(clojure.core/defn pprint [code]
+  (pprint/with-pprint-dispatch
+    pprint/code-dispatch
+    (pprint/pprint code)))
